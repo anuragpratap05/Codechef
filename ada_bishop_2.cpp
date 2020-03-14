f0646b7959cff98c41048552f2ebf87b9daa9480
anuragpratap05 <56753626+anuragpratap05@users.noreply.github.com>
2020-03-14 21:00:53 +0530

    Create ada_bishop_2.cpp

---
+++ ada_bishop_2.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int a,b,x=0,y=0;
+    cin>>a;
+    cin>>b;
+    if(a!=b)
+    {
+    x=a+b;
+    y=x/2;
+    cout<<"27"<<endl;
+    cout<<y<<" "<<y<<endl;
+    cout<<"1"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"1"<<" "<<"3"<<endl;
+    cout<<"3"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"1"<<" "<<"5"<<endl;
+    cout<<"5"<<" "<<"1"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"1"<<" "<<"7"<<endl;
+    cout<<"7"<<" "<<"1"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"2"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"2"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"4"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"4"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"6"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"8"<<" "<<"8"<<endl;
+    }
+    
+    else if(a==1 && b==1)
+    {
+    cout<<"25"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"1"<<" "<<"3"<<endl;
+    cout<<"3"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"1"<<" "<<"5"<<endl;
+    cout<<"5"<<" "<<"1"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"1"<<" "<<"7"<<endl;
+    cout<<"7"<<" "<<"1"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"2"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"2"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"4"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"4"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"6"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"8"<<" "<<"8"<<endl; 
+    }
+    else if(a==8 && b==8)
+    {
+    cout<<"24"<<endl;
+    cout<<"1"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"1"<<" "<<"3"<<endl;
+    cout<<"3"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"1"<<" "<<"5"<<endl;
+    cout<<"5"<<" "<<"1"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"1"<<" "<<"7"<<endl;
+    cout<<"7"<<" "<<"1"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"2"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"2"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"4"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"4"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"6"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"6"<<endl;
+    }
+    
+    else
+    {
+    cout<<"26"<<endl;
+    cout<<"1"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"1"<<" "<<"3"<<endl;
+    cout<<"3"<<" "<<"1"<<endl;
+    cout<<"2"<<" "<<"2"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"1"<<" "<<"5"<<endl;
+    cout<<"5"<<" "<<"1"<<endl;
+    cout<<"3"<<" "<<"3"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"1"<<" "<<"7"<<endl;
+    cout<<"7"<<" "<<"1"<<endl;
+    cout<<"4"<<" "<<"4"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"2"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"2"<<endl;
+    cout<<"5"<<" "<<"5"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"4"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"4"<<endl;
+    cout<<"6"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"6"<<" "<<"8"<<endl;
+    cout<<"8"<<" "<<"6"<<endl;
+    cout<<"7"<<" "<<"7"<<endl;
+    cout<<"8"<<" "<<"8"<<endl;
+    }
+    }
+}
